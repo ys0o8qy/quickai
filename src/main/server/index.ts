8f202e2677f5a2c6c3ed8e6a7329b8dff4a1a13f
ys0o8qh <ys0o8qh@gmail.com>
2025-07-17 23:55:27 +0800

    feat: 正常返回数据

--- src/main/server/index.ts
+++ src/main/server/index.ts
@@ -1,17 +1,19 @@
 import 'dotenv/config'
 import express from 'express'
 import type { Request, Response } from 'express'
-
-import { mastra } from './mastra/index'
 import { streamText } from 'ai'
 import { deepkseekChat } from './mastra/providers'
+import { mastra } from './mastra/index'
+import debug from 'debug'
 
 const app = express()
+const log = debug('server')
+
+app.use(express.json())
 
 app.get('/api/weather', async (req: Request, res: Response): Promise<void> => {
     const { city } = req.query as { city?: string }
 
-    res.header('Access-Control-Allow-Origin', '*')
     if (!city) {
         res.status(400).send("Missing 'city' query parameter")
         return
@@ -23,24 +25,23 @@ app.get('/api/weather', async (req: Request, res: Response): Promise<void> => {
         const result = await agent.generate(`What's the weather like in ${city}?`)
         res.send(result.text)
     } catch (error) {
-        console.error('Agent error:', error)
+        log('Agent error:', error)
         res.status(500).send('An error occurred while processing your request')
     }
 })
 
 app.post('/chat', async (req: Request, res: Response) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    console.error('receive request')
+    const body = req.body
+
+    const messages = body.messages || []
+    log('messages', messages)
     const result = streamText({
         model: deepkseekChat,
-        prompt: 'Invent a new holiday and describe its traditions.'
+        messages: messages
     })
 
     result.pipeDataStreamToResponse(res)
 })
 
-// app.listen(port, () => {
-//     console.log(`Server listening on port ${port}`)
-// })
 
 export default app
