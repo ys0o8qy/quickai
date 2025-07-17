1617d826be9eef2bf3208e197454f7a5d2864bf9
ys0o8qh <ys0o8qh@gmail.com>
2025-07-17 23:23:56 +0800

    feat: delete comment

--- src/main/utils/set-cors.ts
+++ src/main/utils/set-cors.ts
@@ -1,19 +1,5 @@
 import { BrowserWindow } from 'electron'
 
-// function UpsertKeyValue(obj: Record<string, string>, keyToChange: string, value: string[]) {
-//     const keyToChangeLower = keyToChange.toLowerCase()
-//     for (const key of Object.keys(obj)) {
-//         if (key.toLowerCase() === keyToChangeLower) {
-//             // Reassign old key
-//             obj[key] = value
-//             // Done
-//             return
-//         }
-//     }
-//     // Insert at end instead
-//     obj[keyToChange] = value
-// }
-
 export function setCORS(win: BrowserWindow) {
     win.webContents.session.webRequest.onBeforeSendHeaders((details, callback) => {
         const { requestHeaders } = details
