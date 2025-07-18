9a1ef53729e9e863318b11bf06e2c8ed7eb6c278
ys0o8qh <ys0o8qh@gmail.com>
2025-07-18 23:48:49 +0800

    feat: add theme

--- src/renderer/src/pages/App.tsx
+++ src/renderer/src/pages/App.tsx
@@ -1,12 +1,15 @@
 import { TestThread } from '@/components/TestThread'
+import { ThemeProvider } from '@/components/theme-provider'
 import { Button } from '@/components/ui/button'
 
 function App(): React.JSX.Element {
     return (
-        <div className="h-full">
-            <Button>Click me</Button>
-            <TestThread />
-        </div>
+        <ThemeProvider>
+            <div className="h-full">
+                <Button>Click me</Button>
+                <TestThread />
+            </div>
+        </ThemeProvider>
     )
 }
 
