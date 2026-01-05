4cce8557d7db70d71aea98d96fad7a1bf4bbad5b
ys0o8qy <ys0o8qy@gmail.com>
2026-01-05 23:05:53 +0800

    test

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -28,8 +28,7 @@ export default defineConfig({
         },
         resolve: {
             alias: {
-                '@renderer': resolve('src/renderer/src'),
-                '@': resolve('src/renderer/src')
+                '@': resolve('src')
             }
         },
         plugins: [react(), tailwindcss()]
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,27 +5,27 @@ import eslintPluginReactHooks from 'eslint-plugin-react-hooks'
 import eslintPluginReactRefresh from 'eslint-plugin-react-refresh'
 
 export default tseslint.config(
-  { ignores: ['**/node_modules', '**/dist', '**/out'] },
-  tseslint.configs.recommended,
-  eslintPluginReact.configs.flat.recommended,
-  eslintPluginReact.configs.flat['jsx-runtime'],
-  {
-    settings: {
-      react: {
-        version: 'detect'
-      }
-    }
-  },
-  {
-    files: ['**/*.{ts,tsx}'],
-    plugins: {
-      'react-hooks': eslintPluginReactHooks,
-      'react-refresh': eslintPluginReactRefresh
+    { ignores: ['**/node_modules', '**/dist', '**/out'] },
+    tseslint.configs.recommended,
+    eslintPluginReact.configs.flat.recommended,
+    eslintPluginReact.configs.flat['jsx-runtime'],
+    {
+        settings: {
+            react: {
+                version: 'detect'
+            }
+        }
     },
-    rules: {
-      ...eslintPluginReactHooks.configs.recommended.rules,
-      ...eslintPluginReactRefresh.configs.vite.rules
-    }
-  },
-  eslintConfigPrettier
+    {
+        files: ['**/*.{ts,tsx}'],
+        plugins: {
+            'react-hooks': eslintPluginReactHooks,
+            'react-refresh': eslintPluginReactRefresh
+        },
+        rules: {
+            ...eslintPluginReactHooks.configs.recommended.rules,
+            ...eslintPluginReactRefresh.configs.vite.rules
+        }
+    },
+    eslintConfigPrettier
 )
